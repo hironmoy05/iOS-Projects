c539f990da2e4c730100a0aadbfe2c0cdb7bc87b
Hironmoy Dhar <hironmoydhar@gmail.com>
2024-01-11 00:12:37 +0530

    Created meow app same as created with objc-C

---
+++ objective_c_swift/objective_c_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  objective_c_swift
+//
+//  Created by Gaurav Sara on 10/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
