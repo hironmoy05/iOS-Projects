3a0d041c0bfd05d4b228e1223f543e0bc94414dd
Hironmoy Dhar <hironmoydhar@gmail.com>
2024-01-11 00:12:55 +0530

    commit again

--- objective_c_swift/objective_c_swift/ViewController.swift
+++ objective_c_swift/objective_c_swift/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelView: UILabel!
+    var soundID: SystemSoundID = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let soundURL = Bundle.main.url(forResource: "Cat", withExtension: "wav") {
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &soundID)
+        }
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        labelView.isHidden = false
+        
+        AudioServicesPlaySystemSound(soundID)
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(hideLabel), userInfo: nil, repeats: false)
+    }
+    
+    @objc func hideLabel() {
+        labelView.isHidden = true
+    }
 }
 
