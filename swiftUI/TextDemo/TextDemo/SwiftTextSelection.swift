345229ec0d303fa86fc0822967cba655aeb798a7
Hironmoy Dhar <hironmoydhar@gmail.com>
2024-02-22 08:21:07 +0530

    done basic textSelection

---
+++ swiftUI/TextDemo/TextDemo/SwiftTextSelection.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftTextSelection.swift
+//  TextDemo
+//
+//  Created by Gaurav Sara on 22/02/24.
+//
+
+import SwiftUI
+
+struct SwiftTextSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    SwiftTextSelection()
+}
