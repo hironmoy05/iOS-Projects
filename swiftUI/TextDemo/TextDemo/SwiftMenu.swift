db17c7df6f57352d3b199585f8f2f51f940a4fbc
Hironmoy Dhar <hironmoydhar@gmail.com>
2024-02-24 15:10:10 +0530

    done basic menu

---
+++ swiftUI/TextDemo/TextDemo/SwiftMenu.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftMenu.swift
+//  TextDemo
+//
+//  Created by Gaurav Sara on 24/02/24.
+//
+
+import SwiftUI
+
+struct SwiftMenu: View {
+    var body: some View {
+        Menu("Click Me") {
+            Button("One") {
+                
+            }
+            
+            Button("Two") {
+                
+            }
+            
+            Button("Three") {
+                
+            }
+            
+            Button("Four") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    SwiftMenu()
+}
